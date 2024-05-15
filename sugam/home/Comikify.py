cc8f3581b869bde3f420e145a33d6f8e7bead9b3
Yuvraj Singh <yuvraj.11619011621@ipu.ac.in>
2024-05-16 00:33:47 +0530

    Update Comikify.py

--- sugam/home/Comikify.py
+++ sugam/home/Comikify.py
@@ -10,7 +10,7 @@ class Comikify:
     def start(self):
         openai.api_key = self.openai_key
         response = openai.Completion.create(
-            engine="text-davinci-002",
+            engine="gpt-3.5-turbo",
             prompt=self.prompt,
             max_tokens=2048,
             n=1,
